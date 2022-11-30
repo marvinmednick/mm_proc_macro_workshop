707255eeb2d2e93b7a8d4d12e5f0b9be6a68f064
Marvin Mednick <mmednick@gmail.com>
2022-11-30 11:42:03 -0800

    Added printing to see what input and procesisn input did

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,29 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
 use quote::quote;
+use syn::{parse_macro_input,DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _input2 = input.clone();
+    // let input1 = parse_macro_input!(input as DeriveInput);
+    let test_type = quote!(String);
+
+    let output2  = quote!( 
+        struct Command2 { 
+            test : usize,
+            test2 : #test_type,
+        }
+        ).into();
+    eprintln!("output is {:#?}",output2);
+    let parsed_output = parse_macro_input!(output2 as DeriveInput);
+    eprintln!("Parsed output is {:#?}",parsed_output);
 
 
+    // Used in the quasi-quotation below as `#name`.
 
-    return quote!()
+//    eprintln!("Input is {:#?}",input2);
+//    eprintln!("........");
+    //eprintln!("Parsed Input is {:#?}",input1);
+    let output : proc_macro::TokenStream = quote!( impl Command { pub fn builder() {} }; ).into();
+    return output
 }
