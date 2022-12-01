89c876388b855ccb996113fd1711b671884c17b8
Marvin Mednick <mmednick@gmail.com>
2022-12-01 12:57:32 -0800

    Additional data output, but most of struct is hard coded

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,31 +1,31 @@
 use proc_macro::{TokenStream};
-use quote::quote;
-use syn::{parse,parse_macro_input,DeriveInput};
+use quote::{quote,format_ident};
+use syn::{parse_macro_input,DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input2 = input.clone();
+    let _input2 = input.clone();
     let input3 = input.clone();
 
     let parsed_input : DeriveInput = syn::parse(input3).unwrap();
-    let parsed_input1 = parse_macro_input!(input as DeriveInput);
+    let _parsed_input1 = parse_macro_input!(input as DeriveInput);
     
-    
-
-    assert_eq!(parsed_input.ident,"Command");
-    let test_type = quote!(String);
+    let struct_name = parsed_input.ident;
+    let builder_name = format_ident!("{}Builder",struct_name);
+   // assert_eq!(parsed_input.ident,"Command");
+    //let test_type = quote!(String);
 
 
 //    eprintln!("Input is {:#?}",input2);
 //    eprintln!("........");
     if let syn::Data::Struct(d) = parsed_input.data {
         //eprintln!("Parsed Input is {:#?}",d);
-        if let syn::Fields::Named(f) = d.fields {
+        if let syn::Fields::Named(_f) = d.fields {
             //eprintln!("named is {:#?}",f.named);
-            eprintln!("There are {} entries",f.named.len());
-            for x in f.named {
-                eprintln!("Field {}  type {:#?}",x.ident.unwrap(),x.ty);
-            }
+            //eprintln!("There are {} entries",f.named.len());
+        //    for x in f.named {
+         //       eprintln!("Field {}  type {:#?}",x.ident.unwrap(),x.ty);
+          //  }
         }
         else {
             eprintln!("fields are {:#?}",d.fields);
@@ -34,16 +34,25 @@ pub fn derive(input: TokenStream) -> TokenStream {
     else {
         eprintln!("Did not match Parsed Input is {:#?}",parsed_input.data);
     }
-    eprintln!("Original input {:#?}",input2);
+    // eprintln!("Original input {:#?}",input2);
     let output : proc_macro::TokenStream = quote!( 
-        impl Command { 
-            pub fn builder() { }
-            pub fn test1() { }
+         pub struct #builder_name {
+             executable: Option<String>,
+             args: Option<Vec<String>>,
+             env: Option<Vec<String>>,
+             current_dir: Option<String>,
+         }
+         
+        impl #struct_name { 
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
         } 
-        struct newOne {
-            field1 : usize,
-            field2 : #test_type,
-        }
         ).into();
     return output
 }
