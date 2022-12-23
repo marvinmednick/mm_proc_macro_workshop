68585d6fe814f587ab2e9be88e2aaa3c6c1ef702
Marvin Mednick <mmednick@gmail.com>
2022-12-23 12:29:24 -0800

    working on seq -- test 01 passes

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,49 @@
+#![feature(proc_macro_diagnostic)]
 use proc_macro::TokenStream;
+use syn::parse::{Parse, ParseStream, Result};
+use quote::quote ;
+use syn::{parse_macro_input, Ident, LitInt, Token, };
+
+struct Seq { 
+    name : Ident,
+    start: LitInt,
+    end: LitInt,
+}
+
+impl Parse for Seq {
+
+    fn parse(input: ParseStream) -> Result<Self> {
+            
+ //Expec  syn::Ident, Token![in], syn::LitInt, Token![..], syn::LitInt.
+        let name : Ident  = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let start : LitInt  = input.parse()?;
+        input.parse::<Token![..]>()?;
+        let end : LitInt  = input.parse()?;
+        // now need to collect all the content
+        let content;
+        let _braces = syn::braced!(content in input);
+        let _tt = proc_macro2::TokenStream::parse(&content)?;
+        Ok(Seq { name, start, end })
+
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+ //   let _ = input;
 
-    unimplemented!()
+//    eprintln!("Input is {:#?}", input);
+
+    //let parsed = parse_macro_input!(input );
+    
+    let Seq {
+        name,
+        start, 
+        end,
+    } = parse_macro_input!(input as Seq);
+    eprintln!("Name is {} Start is {} End is {}",name, start, end);
+
+
+    quote! { }.into()
 }
