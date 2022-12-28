020d8d316d0be8b68079ec51b49bd0b9a115dbc6
Marvin Mednick <mmednick@gmail.com>
2022-12-28 00:16:52 -0800

    Continued WIP on 03 replacement

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,77 +1,86 @@
 #![feature(proc_macro_diagnostic)]
-use proc_macro::TokenStream;
+//use proc_macro::TokenStream;
+use quote::quote;
 use syn::parse::{Parse, ParseStream, Result};
-use quote::quote ;
-use syn::{parse_macro_input, Ident, LitInt, Token, };
+use syn::{parse_macro_input, Ident, LitInt, Token};
 
-struct Seq { 
-    _name : Ident,
+struct Seq {
+    name: Ident,
     start: LitInt,
     end: LitInt,
-    _contents: proc_macro2::TokenStream,
+    contents: proc_macro2::TokenStream,
+}
+
+fn expand_ts(ts: proc_macro2::TokenStream, name: Ident, index: u64) -> proc_macro2::TokenStream {
+    quote! { compile_error!(concat!("error number ", stringify!(#index))); }
 }
 
 impl Parse for Seq {
-
     fn parse(input: ParseStream) -> Result<Self> {
-            
- //Expec  syn::Ident, Token![in], syn::LitInt, Token![..], syn::LitInt.
-        let name : Ident  = input.parse()?;
+        //Expec  Ident, Token![in], LitInt, Token![..], LitInt.
+        let name: Ident = input.parse()?;
         input.parse::<Token![in]>()?;
-        let start : LitInt  = input.parse()?;
+        let start: LitInt = input.parse()?;
         input.parse::<Token![..]>()?;
-        let end : LitInt  = input.parse()?;
+        let end: LitInt = input.parse()?;
         // now need to collect all the content
         let content;
         let _braces = syn::braced!(content in input);
         let contents = proc_macro2::TokenStream::parse(&content)?;
-//        eprintln!("Content is {:?}",content);
-//        eprintln!("Parsed content is {:?}",contents);
-        Ok(Seq { _name : name, start, end, _contents: contents})
-
+        Ok(Seq {
+            name,
+            start,
+            end,
+            contents,
+        })
     }
 }
 
-
 impl Seq {
-
     fn expand(&self) -> proc_macro2::TokenStream {
-    //    let content = quote!{ #contents };
-
-//        eprintln!("Name is {} Start is {} End is {} contnets is {:#?} ",self.name, self.start, self.end, self.contents);
-//        let mut new = quote! {};
-
         let a = self.start.base10_parse::<u16>().unwrap();
         let b = self.end.base10_parse::<u16>().unwrap();
-        let expanded:  Vec<_> = (a..b).map(|i| { 
-           // let varname = format_ident!("{}",i);
-//            eprintln!("I is {}",i);
-            quote!{ compile_error!(concat!("error number ", stringify!(#i))); }
-            //quote!{ struct sname { #varname : u32 } }
-        }).collect();
-//        for n in a..b {
-//            let sname = format_ident!("xyz_{}",n);
-//            let content = quote!{ struct sname { i : u32 } };
-//
-//            new.extend(content);
-//        }
+        let expanded: Vec<_> = (a..b)
+            .map(|i| self.expand_ts(self.contents.clone(), self.name.clone(), i.into()))
+            .collect();
 
         quote! { #(#expanded)*  }
+    }
+
+    fn expand_ts(
+        &self,
+        ts: proc_macro2::TokenStream,
+        name: Ident,
+        index: u64,
+    ) -> proc_macro2::TokenStream {
+        for tt in ts.into_iter() {
+            self.expand_tt(tt, name.clone(), index.clone());
+        }
+
+        quote! { compile_error!(concat!("error number ", stringify!(#index))); }
+    }
+
+    fn expand_tt(
+        &self,
+        tt: proc_macro2::TokenTree,
+        name: Ident,
+        replace_value: u64,
+    ) -> proc_macro2::TokenStream {
+        let updated_tt = match tt {
+            proc_macro2::TokenTree::Group(ref g) => tt,
+            proc_macro2::TokenTree::Ident(ref id) => {
+                let mut replace_lit = proc_macro2::Literal::u64_unsuffixed(replace_value);
+                replace_lit.set_span(id.span());
+                proc_macro2::TokenTree::Literal(replace_lit)
+            }
+            tt => tt,
+        };
+        std::iter::once(updated_tt).collect()
     }
 }
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
- //   let _ = input;
-
-//    eprintln!("Input is {:#?}", input);
-
-    //let parsed = parse_macro_input!(input );
-    
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let parsed = parse_macro_input!(input as Seq);
-//    eprintln!("Name is {} Start is {} End is {} TT is {:#?} ",parsed.name, parsed.start, parsed.end, parsed.contents);
-//    let content = quote!{ #contents };
-
-
     parsed.expand().into()
 }
