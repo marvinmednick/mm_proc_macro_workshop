58878ad4d8dfe7bd2ec4ef2df2cc26cbf13cb85f
Marvin Mednick <mmednick@gmail.com>
2022-12-29 16:44:09 -0800

    05 passing in full with multiple repeats added in 05a

--- seq/tests/progress.rs
+++ seq/tests/progress.rs
@@ -7,6 +7,7 @@ fn tests() {
     t.pass("tests/04-paste-ident.rs");
     t.pass("tests/04a-paste-ident.rs");
     t.pass("tests/05-repeat-section.rs");
+    t.pass("tests/05a-repeat-section.rs");
     //t.pass("tests/06-init-array.rs");
     //t.pass("tests/07-inclusive-range.rs");
     //t.compile_fail("tests/08-ident-span.rs");
